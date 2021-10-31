2de005ac3ded7d7236c88bf3ce7330e4fb6e2c8a
vsinky <sinkykumariverma@gmail.com>
2021-10-31 22:26:34 +0530

    Added : to play the game with 2 player

--- src/com/bridgelabz/demo/Game.java
+++ src/com/bridgelabz/demo/Game.java
@@ -2,6 +2,8 @@ package com.bridgelabz.demo;
 
 public class Game {
 	final static int MAX_POSITION = 100;
+	static int player1 = 0;
+	static int player2 = 0;
 
 	public static void main(String[] args) {
 		final int NO_PLAY = 0;
@@ -9,6 +11,9 @@ public class Game {
 		final int ladder = 2;
 		int player = 0;
 		int NO_OF_DICE_ROLLED = 0;
+		int turn = 1;
+		int position = 0;
+		String playerName = "";
 
 		while (player < MAX_POSITION) {
 
@@ -20,6 +25,20 @@ public class Game {
 
 			case NO_PLAY:
 				action = "no_play";
+				if (turn == 1) {
+					position = player;
+					player1 = player;
+					player = player2;
+					turn = 2;
+					playerName = "player1";
+				} else {
+					position = player;
+					player2 = player;
+					player = player1;
+					turn = 1;
+					playerName = "player2";
+
+				}
 				break;
 
 			case snake:
@@ -27,24 +46,49 @@ public class Game {
 				int temp1 = player - dice;
 				if (temp1 < 0)
 					player = 0;
+
 				else
 					player = temp1;
-				break;
+
+				if (turn == 1) {
+					position = player;
+					player1 = player;
+					player = player2;
+					turn = 2;
+					playerName = "player1";
+				} else {
+					position = player;
+					player2 = player;
+					player = player1;
+					turn = 1;
+					playerName = "player2";
+				}
 
 			case ladder:
 				action = "ladder";
 				int temp2 = player + dice;
 				if (temp2 <= 100)
 					player = temp2;
-
+				if (turn == 1) {
+					position = player;
+					player1 = player;
+					player = player1;
+					playerName = "player1";
+				} else {
+					position = player;
+					player2 = player;
+					player = player1;
+					playerName = "player2";
+				}
 				break;
 
 			}
 			NO_OF_DICE_ROLLED++;
 
-			System.out.println("Die:" + dice + " \t action: " + action + " \t player position " + player);
+			System.out.println(
+					playerName + "\t Die:" + dice + " \t action: " + action + " \t player position " + position);
 		}
 		System.out.println("Total number of dice rolled to win the game is " + NO_OF_DICE_ROLLED);
-
+		System.out.println(playerName + " has won the game");
 	}
 }
