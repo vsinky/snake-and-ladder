b1a7b0c88f0e5f4d36885c2cb009dadde7ba6384
vsinky <sinkykumariverma@gmail.com>
2021-10-31 21:24:14 +0530

    player rolls the die

--- src/com/bridgelabz/demo/Game.java
+++ src/com/bridgelabz/demo/Game.java
@@ -4,6 +4,9 @@ public class Game {
 	public static void main(String[] args) {
 		int player = 0;
 		System.out.print("Player intitial position: " + player);
+		
+		int dice = (int) ((Math.random() * 10) % 6) + 1;
+		System.out.println("Dice is rolled " + dice);
 
 	}
 }
