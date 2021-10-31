818df5245234a9503a019dbd1c75e5c8870dbd54
vsinky <sinkykumariverma@gmail.com>
2021-10-31 22:15:57 +0530

    Added : position every die roll

--- src/com/bridgelabz/demo/Game.java
+++ src/com/bridgelabz/demo/Game.java
@@ -8,19 +8,22 @@ public class Game {
 		final int snake = 1;
 		final int ladder = 2;
 		int player = 0;
+		int NO_OF_DICE_ROLLED = 0;
 
 		while (player < MAX_POSITION) {
 
 			int dice = (int) ((Math.random() * 10) % 6) + 1;
 
 			int act = (int) ((Math.random() * 10) % 3);
-
+			String action = "";
 			switch (act) {
 
 			case NO_PLAY:
+				action = "no_play";
 				break;
 
 			case snake:
+				action = "snake ";
 				int temp1 = player - dice;
 				if (temp1 < 0)
 					player = 0;
@@ -29,7 +32,7 @@ public class Game {
 				break;
 
 			case ladder:
-
+				action = "ladder";
 				int temp2 = player + dice;
 				if (temp2 <= 100)
 					player = temp2;
@@ -37,9 +40,11 @@ public class Game {
 				break;
 
 			}
-			System.out.println("Dice is rolled " + dice);
-			System.out.println("player position " + player);
+			NO_OF_DICE_ROLLED++;
 
+			System.out.println("Die:" + dice + " \t action: " + action + " \t player position " + player);
 		}
+		System.out.println("Total number of dice rolled to win the game is " + NO_OF_DICE_ROLLED);
+
 	}
 }
