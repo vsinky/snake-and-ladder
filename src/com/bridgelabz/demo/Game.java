c8f270a8f3659a0f2a27c6012b891b6aac029dab
vsinky <sinkykumariverma@gmail.com>
2021-10-31 21:38:58 +0530

    check the players

--- src/com/bridgelabz/demo/Game.java
+++ src/com/bridgelabz/demo/Game.java
@@ -2,11 +2,25 @@ package com.bridgelabz.demo;
 
 public class Game {
 	public static void main(String[] args) {
+		final int NO_PLAY = 0;
+		final int snake = 1;
+		final int ladder = 2;
 		int player = 0;
-		System.out.print("Player intitial position: " + player);
-		
 		int dice = (int) ((Math.random() * 10) % 6) + 1;
+		int act = (int) ((Math.random() * 10) % 3);
+		switch (act) {
+		case NO_PLAY:
+			break;
+		case snake:
+			player = player - dice;
+			break;
+		case ladder:
+			player = player + dice;
+			break;
+
+		}
 		System.out.println("Dice is rolled " + dice);
+		System.out.println("player position " + player);
 
 	}
 }
