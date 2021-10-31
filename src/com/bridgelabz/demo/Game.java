34b858196bc49975e7d2fb850c01dc231b0da356
vsinky <sinkykumariverma@gmail.com>
2021-10-31 21:54:29 +0530

    repeat the player reaches the winning

--- src/com/bridgelabz/demo/Game.java
+++ src/com/bridgelabz/demo/Game.java
@@ -1,26 +1,39 @@
 package com.bridgelabz.demo;
 
 public class Game {
+	final static int MAX_POSITION = 100;
+
 	public static void main(String[] args) {
 		final int NO_PLAY = 0;
 		final int snake = 1;
 		final int ladder = 2;
 		int player = 0;
-		int dice = (int) ((Math.random() * 10) % 6) + 1;
-		int act = (int) ((Math.random() * 10) % 3);
-		switch (act) {
-		case NO_PLAY:
-			break;
-		case snake:
-			player = player - dice;
-			break;
-		case ladder:
-			player = player + dice;
-			break;
+
+		while (player < MAX_POSITION) {
+
+			int dice = (int) ((Math.random() * 10) % 6) + 1;
+
+			int act = (int) ((Math.random() * 10) % 3);
+
+			switch (act) {
+
+			case NO_PLAY:
+				break;
+			case snake:
+				int temp = player - dice;
+				if (temp < 0) {
+					player = 0;
+				} else
+					player = temp;
+				break;
+			case ladder:
+				player = player + dice;
+				break;
+
+			}
+			System.out.println("Dice is rolled " + dice);
+			System.out.println("player position " + player);
 
 		}
-		System.out.println("Dice is rolled " + dice);
-		System.out.println("player position " + player);
-
 	}
 }
