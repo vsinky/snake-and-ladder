4670fa4413d734ecb639d46433fa36576028fce7
vsinky <sinkykumariverma@gmail.com>
2021-10-31 21:15:50 +0530

    intitial position

---
+++ src/com/bridgelabz/demo/Game.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.demo;
+
+public class Game {
+	public static void main(String[] args) {
+		int player = 0;
+		System.out.print("Player intitial position: " + player);
+
+	}
+}
